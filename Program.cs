66d78ba9b6bbede263e65807dbbea24f60bf787a
microchelick43018 <dyatlovkonstan@gmail.com>
2022-04-26 01:45:09 +0300

    Симуляция теперь рассчитывает дисперсию. Теперь для расчёта можно выбирать между Монте Карло (запустить огромное количество спинов) и полный цикл (расчёт всех позиций слота)

--- Program.cs
+++ Program.cs
@@ -8,7 +8,17 @@ namespace SlotTest
         static void Main(string[] args)
         {
             Slot slot = new Slot();
-            slot.Test();
+            Console.WriteLine("1 - Full Cycle");
+            Console.WriteLine("2 - MonteCarlo");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                slot.TestFullCycle();
+            }
+            else
+            {
+                slot.TestMonteCarlo();
+            }
         }
     }
 
@@ -57,44 +67,191 @@ namespace SlotTest
             { 8, 0 },
             { 10, 0 }
         };
-        private double _averageFreespinsWithRetrigger;
-        private const double IterationsCount = 10000000;
-
-        public void Test()
+        private Dictionary<int, int> _bonusRetriggers = new Dictionary<int, int>
         {
-            //CountAmountSlotPositions();
-            double RTP = 0f;
+            { 6, 0 },
+            { 8, 0 },
+            { 10, 0 }
+        };
+        private double _bonusRTP;
+        private Dictionary<double, int> _baseWins = new Dictionary<double, int>();
+        private Dictionary<double, int> _bonusWins = new Dictionary<double, int>();
+        private double _averageFreespinsWithRetrigger;
+        private double _iterationsCount = 100000000;
+        private double _RTP;
+        private int _amountBonusSpins = 0;
+        private bool _isInBonusGame = false;
+
+        public void TestMonteCarlo()
+        {
             double amountWinX = 0f;
-
-            for (int i = 0; i < IterationsCount; i++)
+            double winX;
+            int bonusGamesCount;
+            for (int i = 0; i < _iterationsCount; i++)
             {
-                GenerateNewMatrix();
-                CheckForABonusTrigger();
-                amountWinX += CountWinX();
+                GenerateRibbonsPositionsMonteCarlo();
+                FillMatrix();
+                winX = CountWinX();
+                if (_baseWins.TryGetValue(winX, out int value2) == true)
+                {
+                    _baseWins[winX]++;
+                }
+                else
+                {
+                    _baseWins.Add(winX, 1);
+                }
+                bonusGamesCount = CheckForABonusTrigger();               
+                if (bonusGamesCount > 5)
+                {
+                    _isInBonusGame = true;
+                    StartBonusGame(bonusGamesCount);
+                    _isInBonusGame = false;
+                }
+                amountWinX += winX;
             }
-
             foreach (var item in _hitFrequency)
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    item.Value[i] /= IterationsCount;
+                    item.Value[i] /= _iterationsCount;
                 }
             }
-            ShowBonusGamesInfo();
+            ShowBonusGamesInfoMonteCarlo();
             ShowHitFrequencyTable();
-            RTP = amountWinX / IterationsCount;
-            Console.WriteLine($"RTP основной игры = {RTP}");
-            Console.WriteLine($"RTP всей игры = {RTP + RTP * _averageFreespinsWithRetrigger}");
+            foreach (var item in _baseWins)
+            {
+                _RTP += item.Value * item.Key;
+            }
+            _RTP /= _iterationsCount;
+            Console.WriteLine($"RTP основной игры = {_RTP}");
+            Console.WriteLine($"RTP бонусной игры: {_bonusRTP}");
+            _RTP += _bonusRTP * _averageFreespinsWithRetrigger;
+            Console.WriteLine($"RTP всей игры = {_RTP}");
+            ShowDispersionMonteCarlo();
         }
 
-        //private void CountAmountSlotPositions()
-        //{
-        //    IterationsCount = 1;
-        //    foreach (var ribbon in _ribbons)
-        //    {
-        //        IterationsCount *= ribbon.Count;
-        //    }
-        //}
+        public void TestFullCycle()
+        {
+            CountAmountSlotPositions();
+            double amountWinX = 0f;
+            double winX;
+            for (_mas[0] = 22; _mas[0] != -1; _mas[0]--)
+            {
+                for (_mas[1] = 22; _mas[1] != -1; _mas[1]--)
+                {
+                    for (_mas[2] = 22; _mas[2] != -1; _mas[2]--)
+                    {
+                        for (_mas[3] = 22; _mas[3] != -1; _mas[3]--)
+                        {
+                            for (_mas[4] = 22; _mas[4] != -1; _mas[4]--)
+                            {
+                                GenerateRibbonsPositionsFullCycle();
+                                FillMatrix();
+                                CheckForABonusTrigger();
+                                winX = CountWinX();
+                                if (_baseWins.TryGetValue(winX, out int value2) == true)
+                                {
+                                    _baseWins[winX]++;
+                                }
+                                else
+                                {
+                                    _baseWins.Add(winX, 1);
+                                }
+                                amountWinX += winX;
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var item in _hitFrequency)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    item.Value[i] /= _iterationsCount;
+                }
+            }
+            ShowBonusGamesInfoFullCycle();
+            ShowHitFrequencyTable();
+            _RTP = amountWinX / _iterationsCount;
+            Console.WriteLine($"RTP основной игры = {_RTP}");
+            _RTP *= 1 + _averageFreespinsWithRetrigger;
+            Console.WriteLine($"RTP всей игры = {_RTP}");
+            ShowDispersionFullCycle();
+        }
+        
+        private void ShowDispersionMonteCarlo()
+        {
+            double baseDispersion = 0f;
+            double bonusDisperion = 0f;
+            _baseWins.Remove(0);
+            _bonusWins.Remove(0);
+            foreach (var item in _baseWins)
+            {
+                baseDispersion += (item.Key - _RTP) * (item.Key - _RTP) * item.Value;
+            }
+            baseDispersion /= _iterationsCount;
+            Console.WriteLine($"Дисперсия базовой игры: {baseDispersion}");
+            foreach (var item in _bonusWins)
+            {
+                bonusDisperion += (item.Key - _RTP) * (item.Key - _RTP) * item.Value;
+            }
+            bonusDisperion /= _amountBonusSpins;
+            Console.WriteLine($"Дисперсия бонусной игры: {bonusDisperion}");
+            Console.WriteLine($"Общая дисперсия: {baseDispersion + bonusDisperion * _averageFreespinsWithRetrigger}");
+        }
+
+        private void ShowDispersionFullCycle()
+        {
+            double baseDispersion = 0f;
+            double bonusDisperion = 0f;
+            _baseWins.Remove(0);
+            _bonusWins.Remove(0);
+            foreach (var item in _baseWins)
+            {
+                baseDispersion += (item.Key - _RTP) * (item.Key - _RTP) * item.Value;
+            }
+            baseDispersion /= _iterationsCount;
+            Console.WriteLine($"Дисперсия базовой игры: {baseDispersion}");
+            bonusDisperion = baseDispersion;// барабаны не отличаются
+            Console.WriteLine($"Дисперсия бонусной игры: {bonusDisperion}");
+            Console.WriteLine($"Общая дисперсия: {baseDispersion + bonusDisperion * _averageFreespinsWithRetrigger}");
+        }
+
+        private void StartBonusGame(int freeSpinsCount)
+        {
+            int bonusGamesCount;
+            double winX;
+            for (int i = 0; i < freeSpinsCount; i++)
+            {
+                GenerateRibbonsPositionsMonteCarlo();
+                FillMatrix();
+                bonusGamesCount = CheckForABonusTrigger();
+                winX = CountWinX();
+                if (_bonusWins.TryGetValue(winX, out int value1))
+                {
+                    _bonusWins[winX]++;
+                }
+                else
+                {
+                    _bonusWins.Add(winX, 1);
+                }
+                if (bonusGamesCount > 5)
+                {
+                    _bonusRetriggers[bonusGamesCount]++;
+                    StartBonusGame(bonusGamesCount);
+                }        
+                _bonusRTP += winX;
+            }
+        }
+
+        private void CountAmountSlotPositions()
+        {
+            _iterationsCount = 1;
+            foreach (var ribbon in _ribbons)
+            {
+                _iterationsCount *= ribbon.Count;
+            }
+        }
 
         private void ShowHitFrequencyTable()
         {
@@ -112,7 +269,7 @@ namespace SlotTest
 
         }
 
-        private void CheckForABonusTrigger()
+        private int CheckForABonusTrigger()
         {
             int scattersCount = 0;
             for (int i = 0; i < SlotHeight; i++)
@@ -128,23 +285,25 @@ namespace SlotTest
             if (scattersCount > 2)
             {
                 _bonusGames[scattersCount * 2]++;
+                return scattersCount * 2;
             }
+            return 0;
         }
 
-        private void ShowBonusGamesInfo()
+        private void ShowBonusGamesInfoFullCycle()
         {
             int amountBonusSpins = 0;
             int amountBonusTriggers = 0;
-            double averageFreespinsWithoutRetrigger = 0;
+            double averageFreespinsWithoutRetrigger;
             foreach (var item in _bonusGames)
             {
                 amountBonusSpins += item.Key * item.Value;
                 amountBonusTriggers += item.Value;
             }
-            Console.WriteLine($"Вероятность фриспинов за спин = {amountBonusTriggers / IterationsCount}");
-            averageFreespinsWithoutRetrigger = amountBonusSpins / IterationsCount;
+            Console.WriteLine($"Вероятность фриспинов за спин = {amountBonusTriggers / _iterationsCount}");
+            averageFreespinsWithoutRetrigger = amountBonusSpins / _iterationsCount;
             Console.WriteLine($"Среднее количество фриспинов за 1 спин без ретриггера = {averageFreespinsWithoutRetrigger}");
-            _averageFreespinsWithRetrigger = amountBonusSpins / IterationsCount;
+            _averageFreespinsWithRetrigger = amountBonusSpins / _iterationsCount;
             _averageFreespinsWithRetrigger = averageFreespinsWithoutRetrigger;
             double temp = averageFreespinsWithoutRetrigger;
             for (int i = 0; i < 1000; i++)
@@ -152,6 +311,27 @@ namespace SlotTest
                 temp *= averageFreespinsWithoutRetrigger;
                 _averageFreespinsWithRetrigger += temp;
             }
+            Console.WriteLine($"С ретриггером: {_averageFreespinsWithRetrigger}");
+        }
+
+        private void ShowBonusGamesInfoMonteCarlo()
+        {
+            int amountBonusTriggers = 0;
+            int amountBonusSpinsRetriggers = 0;
+            foreach (var item in _bonusGames)
+            {
+                _amountBonusSpins += item.Key * item.Value;
+                amountBonusTriggers += item.Value;
+            }
+            Console.WriteLine($"Вероятность фриспинов за спин = {amountBonusTriggers / _iterationsCount}");
+            int amountRetriggerSpins = 0;
+            foreach (var item in _bonusRetriggers)
+            {
+                amountRetriggerSpins += item.Key * item.Value;
+            }
+            Console.WriteLine($"Среднее количество фриспинов за 1 спин без ретриггера = {(_amountBonusSpins-amountRetriggerSpins) / _iterationsCount}");
+            _bonusRTP /= _amountBonusSpins;
+            _averageFreespinsWithRetrigger = (_amountBonusSpins) / _iterationsCount;
             Console.WriteLine($"С ретриггером: {_averageFreespinsWithRetrigger}");
         }
 
@@ -187,7 +367,7 @@ namespace SlotTest
                         paylinesCount *= similuarValuesCount;
                     }
                 }
-                if (multiple > 2 && firstValues[i1] > 2)
+                if (multiple > 2 && firstValues[i1] > 2 && _isInBonusGame == false)
                     _hitFrequency[firstValues[i1]][multiple - 3] += paylinesCount;
                 winX += GetWinXFromPayTable(firstValues[i1], multiple) * paylinesCount;
             }
@@ -224,12 +404,6 @@ namespace SlotTest
             }
         }
 
-        private void GenerateNewMatrix()
-        {
-            GenerateRibbonsPositions();
-            FillMatrix();
-        }
-
         private void FillMatrix()
         {
             for (int i = 0; i < SlotWidth; i++)
@@ -241,16 +415,19 @@ namespace SlotTest
             }
         }
 
-        private void GenerateRibbonsPositions()
+        private void GenerateRibbonsPositionsMonteCarlo()
         {
-            //for (int i = 0; i < SlotWidth; i++)
-            //{
-            //    _ribbonsPositions[i] = _mas[i];
-            //}
-            Random random = new Random();
             for (int i = 0; i < SlotWidth; i++)
             {
-                _ribbonsPositions[i] = random.Next(0, _ribbons[i].Count);
+                _ribbonsPositions[i] = _random.Next(0, _ribbons[i].Count);
+            }
+        }
+
+        private void GenerateRibbonsPositionsFullCycle()
+        {
+            for (int i = 0; i < SlotWidth; i++)
+            {
+                _ribbonsPositions[i] = _mas[i];
             }
         }
     }
